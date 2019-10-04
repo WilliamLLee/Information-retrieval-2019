57a652abfbab69f781be5143c05049e4050ef7cd
Jack-Lio <lio6218@163.com>
2019-10-04 23:31:46 +0800

    前端完成，整体功能全部完成

--- pro1/mail_search_system/main.py
+++ pro1/mail_search_system/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)  # 实例化app对象
 CORS(app,supports_credentials=True)    # 解决跨域请求无响应问题
 
 
-def tran_json(res):
+def tran_result(res):
     f_read = open('..\mail_list.txt','r',encoding='utf-8')  #打开文件映射表
     file_list = {}          # 将文件映射表内容存在这个2*x维列表中
     for line in f_read :
@@ -19,39 +19,44 @@ def tran_json(res):
 
     res_j = []
     for item in  res:
-        res_j.append(
-            {
-                'path': file_list[item[0]],
-                'score': item[1]
-            }
-        )
-    return  jsonify(res_j)
+        if item[1]>0:           # 结果只输出评分不为0的
+            res_j.append(
+                {
+                    'path': file_list[item[0]],
+                    'score': item[1]
+                }
+            )
+
+    return  res_j
 
 
 @app.route('/',methods=['GET','POST'])
 def query():
-    print(request.form)
+    # print(request.form)
     test_sentence = request.form['sentence']
     type_s = request.form['type_s']
     result = querry(test_sentence, index_dict, words_count_of_file, type_s)
-    print( tran_json(result))
-    return tran_json(result)
+    # print( tran_result(result))
+    return jsonify(tran_result(result))
 
-@app.route('/recording',methods=['GET','POST'])
-def recoding():
-    return get_audio(in_path)
+
 
 @app.route('/audio',methods=['GET','POST'])
 def audioQuery():
+    get_audio(in_path)
     signal = open(in_path, "rb").read()
     rate = 16000
     token = get_token()
-    test_sentence = recognize(signal, rate, token)['result'][0]
+    rec = recognize(signal, rate, token)
+    if 'result' in rec.keys():
+        test_sentence = rec['result'][0]
+    else:
+        return json.dumps({"error": 'no input'}), 500
     print(test_sentence)
     type_s = request.form['type_s']
     result = querry(test_sentence, index_dict, words_count_of_file, type_s)
-    print( tran_json(result))
-    return jsonify(result)
+    # print( tran_result(result))
+    return  jsonify({'sentence':test_sentence,'result':tran_result(result)})
 
 if __name__ == '__main__':
     app.run(debug=True)
